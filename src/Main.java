4810b225c8c3be568192456b2964cea3d79ff03a
YukhnikOlga <olga.iukhnik@gmail.com>
2024-04-27 14:01:15 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int account = 100; // начальный счет миль
         int ticket = 5_000; // стоимость авиабилета
         int miles = ticket / 20; // расчет миль 1/20
-        int total = account + miles;
+        int total = miles;
 
-        System.out.println("Счет: " + account);
         System.out.println("Сумма покупки: " + ticket);
         System.out.println("Начислено миль: " + miles);
         System.out.println("Текущий счет: " + total);
